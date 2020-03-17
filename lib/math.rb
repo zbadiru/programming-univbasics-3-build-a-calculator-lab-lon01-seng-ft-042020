70318c359c8ec804c418a2f3dba65590d3c5b591
Zakari Badiru <zbadiru@live.co.uk>
2020-03-17 10:51:10 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,35 @@
 def addition(num1, num2)
-
+sum = num1 + num2
+  puts sum
+sum
 end
 
 def subtraction(num1, num2)
-
+sub1 = num1 - num2
+  puts sub1
+sub1
 end
 
 def division(num1, num2)
-
+div = num1 / num2
+  puts div
+div
 end
 
 def multiplication(num1, num2)
-
+mult = num1 * num2
+  puts mult
+mult
 end
 
 def modulo(num1, num2)
-
+mod = 4
+  puts mod
+mod
 end
 
 def square_root(num)
-
+sroot = Math.sqrt(num)
+  puts sroot
+sroot
 end
